c94af821538d07821d6945f7232108b48729a655
Danqi Wang <beyondwdq@gmail.com>
2011-04-15 10:29:23 +0800

    add file edsim_naive.h

---
+++ edsim_naive/edsim_naive.h
@@ -0,0 +1,24 @@
+#ifndef __EDSIM_NAIVE_H
+#define __EDSIM_NAIVE_H
+
+
+//Implementation of Event
+typedef void (*TEventHandler) (void);
+
+struct Event{
+    Event(double occur_time, TEventHandler handler)
+    : m_occur_time(occur_time)
+    , m_handler(handler)
+    {}
+
+    bool operator < (const Event& rhs) const{
+        return m_occur_time < rhs.m_occur_time;
+    }
+
+    double m_occur_time;
+    TEventHandler m_handler;
+};
+
+void enqueue(const Event& event);
+
+#endif
